af157123ac429544d36d3628154de1191a24362d
mcqnyc <mcqnyc@gmail.com>
2024-12-08 22:01:53 -0500

    clean up comments etc

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { middleware, errorMiddleware } = require('@envoy/envoy-integrations-sdk')
 const app = express();
 app.use(middleware());
 
-let maxVisitDurationLocal = Infinity;
 
 app.post('/visit-duration', (req, res) => {
   res.send([
@@ -15,6 +14,8 @@ app.post('/visit-duration', (req, res) => {
   ]);
 });
 
+
+let maxVisitDurationLocal = Infinity;
 app.post('/validate-me', (req, res) => {
 // app.post('/validate-me', async (req, res) => {
   const envoy = req.envoy
@@ -25,11 +26,7 @@ app.post('/validate-me', (req, res) => {
     // await installStorage.set('maxVisitDuration', maxVisitDuration);
     // const { value } = await installStorage.get('maxVisitDuration');
     // console.log('maxVisitDuration value:', value)
-    console.log('here!')
     maxVisitDurationLocal = maxVisitDuration;
-    console.log('maxVisitDurationLocal: ', maxVisitDurationLocal)
-    // const job = envoy.job;
-    // await job.attach({ label: 'maxVisitDuration', value: maxVisitDuration }); // show in the Envoy dashboard.
 
     res.send({ message: 'Success!', message: 'test'});
     // res.send({ maxVisitDuration: maxVisitDuration, message: 'Success!'});
@@ -37,6 +34,7 @@ app.post('/validate-me', (req, res) => {
     res.sendFailed('These values are bad: the duration should be between 0 and 180 minutes');
   }
 });
+
 
 app.post('/visitor-sign-in', async (req, res) => {
   const envoy = req.envoy;
@@ -48,40 +46,18 @@ app.post('/visitor-sign-in', async (req, res) => {
 
 
 app.post('/visitor-sign-out', async (req, res) => {
-  // const envoy = req.envoy; // our middleware adds an "envoy" object to req.
-  // const job = envoy.job;
-  // const goodbye = envoy.meta.config.GOODBYE;
-  // const visitor = envoy.payload;
-  // const visitorName = visitor.attributes['full-name'];
   const envoy = req.envoy
-  const installStorage = envoy.installStorage
-  // console.log('envoy:', envoy)
-  // console.log('envoy body:', envoy.body)
-  // console.log('envoy > body > payload:', envoy.body.payload)
-  // console.log('envoy > body > payload > attribs:', envoy.body.payload.attributes)
-  // console.log('envoy > body > meta >:', envoy.body.meta)
-  // console.log('envoy > body > meta > config:', envoy.body.meta.config)
-  // console.log('envoy > body > env > config:', envoy.body.env.config)
-  // const maxVisitDuration = envoy.payload.MaxVisitDuration
-  // console.log('maxVisitDuration:', maxVisitDuration)
   const attributes = envoy.payload.attributes
   const signInTime = attributes['signed-in-at']
   const signOutTime = attributes['signed-out-at']
-  console.log('signInTime:', signInTime)
-  console.log('signOutTime:', signOutTime)
-  
+
+  const installStorage = envoy.installStorage
   // const { maxVisitDuration } = await installStorage.get('maxVisitDuration');
-  // console.log('maxVisitDuration sig out:', maxVisitDuration)
-  console.log('maxVisitDurationLocal sig out:', maxVisitDurationLocal)
-  
+
   const signIn = new Date(signInTime);
-  console.log('signIn:', signIn)
   const signOut = new Date(signOutTime);
-  console.log('signOut:', signOut)
   const maxDurationInMillseconds = maxVisitDurationLocal * 60 * 1000;
-  console.log('maxDurationInMillseconds:', maxDurationInMillseconds)
   const differenceInMilliseconds = signOut - signIn;
-  console.log('differenceInMilliseconds:', differenceInMilliseconds)
 
   if (maxDurationInMillseconds < differenceInMilliseconds) {
     res.sendFailed('The visitor overstayed their alloted time.');
@@ -90,12 +66,6 @@ app.post('/visitor-sign-out', async (req, res) => {
   }
 });
 
-
-app.use(function (req, res, next) {
-  console.log('Time:', Date.now())
-  console.log('======')
-  next()
-})
 
 app.use(errorMiddleware());
 
